5f04a2550eb7b22450351c8d10e39d831063ab03
james <james@james-ThinkPad-T430s.mints.com>
2016-05-04 08:26:21 +0800

    fix configuration for ceilometer

--- prestack.sh
+++ prestack.sh
@@ -2,7 +2,7 @@
 # change IP in local.conf
 echo 'zouzhicheng' | sudo losetup /dev/loop1 /home/james/sdb1
 #echo 'zouzhicheng' | sudo ovs-vsctl add-br br-ex
-#echo 'zouzhicheng' | sudo ovs-vsctl add-port br-ex eth0
+#echo 'zouzhicheng' | sudo ovs-vsctl add-port br-ex wlan0
 #cd  /etc/apache2/sites-enabled
 #echo 'zouzhicheng' | sudo ln -s ../sites-available/keystone.conf keystone.conf
 #echo 'zouzhicheng' | sudo service apache2 restart 
